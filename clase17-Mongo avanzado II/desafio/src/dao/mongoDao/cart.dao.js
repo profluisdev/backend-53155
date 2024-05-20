30f4babbd27bb519e4c15fb1f66823bbb408313e
Luis Mera <profeluismeradev@gmail.com>
2024-05-20 20:15:55 -0300

    after para completar la segunda pre entrega

--- clase17-Mongo avanzado II/desafio/src/dao/mongoDao/cart.dao.js
+++ clase17-Mongo avanzado II/desafio/src/dao/mongoDao/cart.dao.js
@@ -13,29 +13,11 @@ const create = async (data) => {
 
 const addProductToCart = async (cid, pid) => {
   const product = await productModel.findById(pid);
-  if (!product)
-    return {
-      product: false,
-    };
-
-  const productInCart = await cartModel.findOneAndReplace({ _id: cid, "products.product": pid }, { $inc: { "products.$.quantity": 1 } });
-  if (!productInCart) {
-    await cartModel.findOneAndUpdate(cid, { $push: { products: { product: pid, quantity: 1 } } });
-  }
-
+  if (!product) return { product: false };
   const cart = await cartModel.findById(cid);
+  if (!cart) return { cart: false };
 
-  return cart;
-};
-
-const deleteProductInCart = async (cid, pid) => {
-  const product = await productModel.findById(pid);
-  if (!product)
-    return {
-      product: false,
-    };
-
-  const cart = await cartModel.findOneAndUpdate({ _id: cid, "products.product": pid }, { $inc: { "products.$.quantity": -1 } });
+  const productInCart = await cartModel.findOneAndUpdate({ _id: cid, "products.product": pid }, { $inc: { "products.$.quantity": 1 } });
   /* 
   $inc: Este es el operador de incremento. Se utiliza para incrementar el valor de un campo numérico en la cantidad especificada.
   "products.$.quantity": 
@@ -44,15 +26,56 @@ const deleteProductInCart = async (cid, pid) => {
   en el filtro de la consulta. Básicamente, este operador selecciona el elemento correcto del array para la actualización.
   quantity: es el campo del objeto dentro del array products cuyo valor queremos incrementar.
   */
-  if (!cart)
-    return {
-      cart: false,
-    };
+
+  if (!productInCart) {
+    await cartModel.updateOne({ _id: cid }, { $push: { products: { product: pid, quantity: 1 } } });
+  }
+
+  const cartUpdate = await cartModel.findById(cid);
+  // const cartUpdate = await cartModel.findById(cid).populate("products.product");
+  return cartUpdate;
+};
+
+const deleteProductInCart = async (cid, pid) => {
+  const product = await productModel.findById(pid);
+  if (!product) return { product: false };
+  const cart = await cartModel.findOneAndUpdate({ _id: cid, "products.product": pid }, { $inc: { "products.$.quantity": -1 } });
+  if (!cart) return { cart: false };
+  const cartUpdate = await cartModel.findById(cid);
+  return cartUpdate;
+};
+
+const update = async (cid, data) => {
+  await cartModel.updateOne({ _id: cid }, { $set: { products: [] } });
+  await cartModel.updateOne({ _id: cid }, { $set: { products: data } });
+  const cart = await cartModel.findById(cid);
+  return cart;
+};
+
+const updateQuantityProductInCart = async (cid, pid, quantity) => {
+  const product = await productModel.findById(pid);
+  if (!product) return { product: false };
+
+  const cart = await cartModel.findOneAndUpdate({ _id: cid, "products.product": pid }, { $set: { "products.$.quantity": quantity } });
+  if (!cart) return { cart: false };
+
+  const cartUpdate = await cartModel.findById(cid);
+  return cartUpdate;
+};
+
+const deleteAllProductsInCart = async (cid) => {
+  const cart = await cartModel.findByIdAndUpdate(cid, { $set: { products: [] } });
+
+  const cartUpdate = await cartModel.findById(cid);
+  return cartUpdate;
 };
 
 export default {
   getById,
   create,
   addProductToCart,
-  deleteProductInCart
+  deleteProductInCart,
+  update,
+  updateQuantityProductInCart,
+  deleteAllProductsInCart,
 };
